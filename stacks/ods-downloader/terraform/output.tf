b03e3cf6cd39795321c9cc87de90aaa307202af4
eyupaksu-nhs <eyup.aksu@nhs.net>
2021-05-07 14:10:57 +0100

    PRMT-1797 [HZ/EA] Export ods downloader iam role to use in stepfunction

--- stacks/ods-downloader/terraform/output.tf
+++ stacks/ods-downloader/terraform/output.tf
@@ -4,3 +4,10 @@ resource "aws_ssm_parameter" "ods_downloader_task_definition_arn" {
   value = aws_ecs_task_definition.ods_downloader.arn
   tags  = local.common_tags
 }
+
+resource "aws_ssm_parameter" "ods_downloader_iam_role_arn" {
+  name  = "/registrations/${var.environment}/data-pipeline/ods-downloader/iam-role-arn"
+  type  = "String"
+  value = aws_iam_role.ods_downloader.arn
+  tags  = local.common_tags
+}
