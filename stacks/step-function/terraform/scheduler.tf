862db2771d67481d3a9b4ca66fc3a5fb67ed33a4
eyupaksu-nhs <eyup.aksu@nhs.net>
2021-05-12 10:39:27 +0100

    PRMT-1797 [KV/EA] Rename cloudwatch event scheduler cron expression.

--- stacks/step-function/terraform/scheduler.tf
+++ stacks/step-function/terraform/scheduler.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_event_target" "data_pipeline" {
   target_id = "${var.environment}-data-pipeline"
-  rule      = aws_cloudwatch_event_rule.run_every_three_mins.id
+  rule      = aws_cloudwatch_event_rule.run_once_a_month_cron_expression.id
   arn       = aws_sfn_state_machine.data_pipeline.arn
   role_arn  = aws_iam_role.data_pipeline_trigger.arn
   input_transformer {
@@ -17,9 +17,9 @@ resource "aws_cloudwatch_event_target" "data_pipeline" {
   }
 }
 
-resource "aws_cloudwatch_event_rule" "run_every_three_mins" {
+resource "aws_cloudwatch_event_rule" "run_once_a_month_cron_expression" {
   name        = "${var.environment}-data-pipeline-trigger"
-  description = "Trigger Step Function"
+  description = "Trigger Step Function with the cron expression"
   schedule_expression = "cron(0/0 1 15 * ? *)"
   tags                = local.common_tags
 }
