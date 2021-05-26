dd990b81f4f1a7c6cbf103ae6b323659f8dcfef0
Jake Newman <jacob.newman1@nhs.net>
2021-05-26 11:06:17 +0100

    PRMT-1798 [JN/JG] Add spine-exporter s3 bucket for raw spine input files

---
+++ stacks/spine-exporter/terraform/s3.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "spine_exporter" {
+  bucket = "prm-gp2gp-raw-spine-data-${var.environment}"
+  acl    = "private"
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "Raw spine data"
+    }
+  )
+}
+
+resource "aws_s3_bucket_public_access_block" "spine_exporter" {
+  bucket = aws_s3_bucket.spine_exporter.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
