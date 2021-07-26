06698d5a971e0dfb9f9efd95ce8a11e3e250bf3d
helen.zhou <helen.zhou1@nhs.net>
2021-07-26 10:33:07 +0100

    PRMT-2109 [HZ/KV] Add S3 bucket to store transfer classifer output

---
+++ stacks/transfer-classifier/terraform/s3.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "transfer_classifier" {
+  bucket = "prm-gp2gp-transfer-data-${var.environment}"
+  acl    = "private"
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "Output transfer data for metrics calculator and data analysis"
+    }
+  )
+}
+
+resource "aws_s3_bucket_public_access_block" "transfer_classifier" {
+  bucket = aws_s3_bucket.transfer_classifier.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
