7613f3958ed9553954be2c98a86e2343c3fa15ca
helen.zhou <helen.zhou1@nhs.net>
2021-07-28 11:11:19 +0100

    PRMT-2109 [HZ/JN] Add Transfer Classifier as new Step Function step

--- stacks/step-function/terraform/step-function.tf
+++ stacks/step-function/terraform/step-function.tf
@@ -42,6 +42,40 @@ resource "aws_sfn_state_machine" "data_pipeline" {
             ]
           }
         },
+        "Next" : "TransferClassifier"
+      },
+      "TransferClassifier" : {
+        "Type" : "Task",
+        "Comment" : "Transfer Classifier - responsible for taking raw spine transfer data and organisation meta data and allocating transfers a status",
+        "Resource" : "arn:aws:states:::ecs:runTask.sync",
+        "ResultPath" : null,
+        "Parameters" : {
+          "LaunchType" : "FARGATE",
+          "Cluster" : data.aws_ssm_parameter.data_pipeline_ecs_cluster_arn.value,
+          "TaskDefinition" : data.aws_ssm_parameter.transfer_classifier_task_definition_arn.value,
+          "NetworkConfiguration" : {
+            "AwsvpcConfiguration" : {
+              "Subnets" : [
+                data.aws_ssm_parameter.data_pipeline_private_subnet_id.value
+              ],
+              "SecurityGroups" : [
+              data.aws_ssm_parameter.outbound_only_security_group_id.value],
+            }
+          },
+          "Overrides" : {
+            "ContainerOverrides" : [
+              {
+                "Name" : "transfer-classifier",
+                "Environment" : [
+                  {
+                    "Name" : "DATE_ANCHOR",
+                    "Value.$" : "$.time"
+                  }
+                ],
+              }
+            ]
+          }
+        },
         "Next" : "MetricsCalculator"
       },
       "MetricsCalculator" : {
