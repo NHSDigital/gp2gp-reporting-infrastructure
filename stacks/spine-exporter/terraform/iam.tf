8a533b5d5b48f9708b5a226fbeb6f7d3a4c148fb
helen.zhou <helen.zhou1@nhs.net>
2021-12-07 15:17:45 +0000

    PRMT-1768 [HZ/DS] Add IAM policy to role that allows spine exporter task to write to S3

--- stacks/spine-exporter/terraform/iam.tf
+++ stacks/spine-exporter/terraform/iam.tf
@@ -2,6 +2,10 @@ resource "aws_iam_role" "spine_exporter" {
   name               = "${var.environment}-registrations-spine-exporter"
   description        = "Role for spine exporter ECS task"
   assume_role_policy = data.aws_iam_policy_document.ecs_assume.json
+  managed_policy_arns = [
+    aws_iam_policy.spine_exporter_output_bucket_write_access.arn,
+    aws_iam_policy.ssm_access.arn
+  ]
 }
 
 data "aws_iam_policy_document" "ecs_assume" {
@@ -15,11 +19,6 @@ data "aws_iam_policy_document" "ecs_assume" {
       ]
     }
   }
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_access" {
-  role       = aws_iam_role.spine_exporter.name
-  policy_arn = aws_iam_policy.ssm_access.arn
 }
 
 resource "aws_iam_policy" "ssm_access" {
@@ -46,3 +45,22 @@ locals {
 }
 
 data "aws_caller_identity" "current" {}
+
+resource "aws_iam_policy" "spine_exporter_output_bucket_write_access" {
+  name   = "${aws_s3_bucket.spine_exporter.bucket}-write"
+  policy = data.aws_iam_policy_document.spine_exporter_output_bucket_write_access.json
+}
+
+data "aws_iam_policy_document" "spine_exporter_output_bucket_write_access" {
+  statement {
+    sid = "WriteObjects"
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.spine_exporter.bucket}/*"
+    ]
+  }
+}
