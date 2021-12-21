94b0283d310b22c5bd779b508ebf65d29c890651
Dorota-TW <dorota.sobkow1@nhs.net>
2021-12-21 09:33:38 +0000

    PRMT-2421 [HZ/DS] Fix cron schedule

--- stacks/spine-exporter/terraform/scheduler.tf
+++ stacks/spine-exporter/terraform/scheduler.tf
@@ -51,7 +51,7 @@ data "aws_iam_policy_document" "run_task" {
 resource "aws_cloudwatch_event_rule" "ecs_event_rule" {
   name                = "run-spine-exporter-2am-every-morning"
   description         = "Cloudwatch Event Rule that runs Spine Exporter ECS task 2am every morning"
-  schedule_expression = "cron(0 2 * * * *)"
+  schedule_expression = "cron(0 2 * * ? *)"
 
   tags = merge(
     local.common_tags,
