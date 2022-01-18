ff4739a209be62703f50069bf0c3bb570f8b2a38
Serge Pogadaev <serge.pogadaev1@nhs.net>
2022-01-18 14:30:21 +0000

    PRMT-2486 [HZ/SP] Add conditional stage to transfer classifier

--- stacks/step-function/terraform/step-function.tf
+++ stacks/step-function/terraform/step-function.tf
@@ -164,9 +164,28 @@ resource "aws_sfn_state_machine" "transfer_classifer" {
     }
   )
   definition = jsonencode({
-    "StartAt" : "TransferClassifier",
+    "StartAt" : "Has Start and End Datetime",
     "States" : {
-      "TransferClassifier" : {
+      "Has Start and End Datetime": {
+        "Type": "Choice",
+        "Choices": [
+          {
+            "And": [
+              {
+                "Variable": "$.START_DATETIME",
+                "IsPresent": true
+              },
+              {
+                "Variable": "$.END_DATETIME",
+                "IsPresent": true
+              }
+            ],
+            "Next": "TransferClassifier (with start/end datetime)"
+          }
+        ],
+        "Default": "TransferClassifier (without start/end datetime)"
+      },
+      "TransferClassifier (with start/end datetime)" : {
         "Type" : "Task",
         "Comment" : "Transfer Classifier - responsible for taking raw spine transfer data and organisation meta data and allocating transfers a status",
         "Resource" : "arn:aws:states:::ecs:runTask.sync",
@@ -197,6 +216,48 @@ resource "aws_sfn_state_machine" "transfer_classifer" {
                     "Name" : "END_DATETIME",
                     "Value.$" : "$.END_DATETIME"
                   },
+                  {
+                    "Name" : "INPUT_SPINE_DATA_BUCKET",
+                    "Value.$" : "$.INPUT_SPINE_DATA_BUCKET"
+                  },
+                  {
+                    "Name" : "OUTPUT_TRANSFER_DATA_BUCKET",
+                    "Value.$" : "$.OUTPUT_TRANSFER_DATA_BUCKET"
+                  },
+                  {
+                    "Name" : "CONVERSATION_CUTOFF_DAYS",
+                    "Value.$" : "$.CONVERSATION_CUTOFF_DAYS"
+                  }
+                ],
+              }
+            ]
+          }
+        },
+        "End" : true
+      },
+      "TransferClassifier (without start/end datetime)" : {
+        "Type" : "Task",
+        "Comment" : "Transfer Classifier - responsible for taking raw spine transfer data and organisation meta data and allocating transfers a status",
+        "Resource" : "arn:aws:states:::ecs:runTask.sync",
+        "ResultPath" : null,
+        "Parameters" : {
+          "LaunchType" : "FARGATE",
+          "Cluster" : data.aws_ssm_parameter.data_pipeline_ecs_cluster_arn.value,
+          "TaskDefinition" : data.aws_ssm_parameter.transfer_classifier_task_definition_arn.value,
+          "NetworkConfiguration" : {
+            "AwsvpcConfiguration" : {
+              "Subnets" : [
+                data.aws_ssm_parameter.data_pipeline_private_subnet_id.value
+              ],
+              "SecurityGroups" : [
+              data.aws_ssm_parameter.outbound_only_security_group_id.value],
+            }
+          },
+          "Overrides" : {
+            "ContainerOverrides" : [
+              {
+                "Name" : "transfer-classifier",
+                "Environment" : [
                   {
                     "Name" : "INPUT_SPINE_DATA_BUCKET",
                     "Value.$" : "$.INPUT_SPINE_DATA_BUCKET"
