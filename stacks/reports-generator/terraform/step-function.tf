de8c2b01505b1443f242cae2e100fb7175f2fda7
Jake Newman <jacob.newman1@nhs.net>
2022-02-04 14:18:19 +0000

    PRMT-2488 [JN] Determine which reporting window in reports generator depending on input

--- stacks/reports-generator/terraform/step-function.tf
+++ stacks/reports-generator/terraform/step-function.tf
@@ -10,7 +10,85 @@ resource "aws_sfn_state_machine" "reports_generator" {
   definition = jsonencode({
     "StartAt" : "GenerateReport",
     "States" : {
-      "GenerateReport" : {
+      "Determining reporting window" : {
+        "Type" : "Choice",
+        "Choices" : [
+          {
+            "And" : [
+              {
+                "Variable" : "$.START_DATETIME",
+                "IsPresent" : true
+              },
+              {
+                "Variable" : "$.END_DATETIME",
+                "IsPresent" : true
+              }
+            ],
+            "Next" : "Custom reporting window"
+          },
+          {
+            "Variable" : {
+              "Variable" : "$.NUMBER_OF_DAYS",
+              "IsPresent" : true
+            },
+            "Next" : "Daily/weekly reporting window"
+          },
+          {
+            "Variable" : {
+              "Variable" : "$.NUMBER_OF_MONTHS",
+              "IsPresent" : true
+            },
+            "Next" : "Monthly reporting window"
+          }
+        ]
+      },
+      "Custom reporting window" : {
+        "Type" : "Task",
+        "Comment" : "Reports Generator - creates a specific report needed for analysing GP2GP transfers",
+        "Resource" : "arn:aws:states:::ecs:runTask.sync",
+        "ResultPath" : null,
+        "Parameters" : {
+          "LaunchType" : "FARGATE",
+          "Cluster" : data.aws_ssm_parameter.data_pipeline_ecs_cluster_arn.value,
+          "TaskDefinition" : data.aws_ssm_parameter.reports_generator_task_definition_arn.value,
+          "NetworkConfiguration" : {
+            "AwsvpcConfiguration" : {
+              "Subnets" : [
+                data.aws_ssm_parameter.data_pipeline_private_subnet_id.value
+              ],
+              "SecurityGroups" : [
+                data.aws_ssm_parameter.outbound_only_security_group_id.value],
+            }
+          },
+          "Overrides" : {
+            "ContainerOverrides" : [
+              {
+                "Name" : "reports-generator",
+                "Environment" : [
+                  {
+                    "Name" : "REPORT_NAME",
+                    "Value.$" : "$.REPORT_NAME"
+                  },
+                  {
+                    "Name" : "CONVERSATION_CUTOFF_DAYS",
+                    "Value.$" : "$.CONVERSATION_CUTOFF_DAYS"
+                  },
+                  {
+                    "Name" : "START_DATETIME",
+                    "Value.$" : "$.START_DATETIME"
+                  },
+                  {
+                    "Name" : "END_DATETIME",
+                    "Value.$" : "$.END_DATETIME"
+                  },
+                ],
+              }
+            ]
+          }
+        },
+        "End" : true
+      },
+      "Daily/weekly reporting window" : {
         "Type" : "Task",
         "Comment" : "Reports Generator - creates a specific report needed for analysing GP2GP transfers",
         "Resource" : "arn:aws:states:::ecs:runTask.sync",
@@ -44,6 +122,48 @@ resource "aws_sfn_state_machine" "reports_generator" {
                   {
                     "Name" : "NUMBER_OF_DAYS",
                     "Value.$" : "$.NUMBER_OF_DAYS"
+                  },
+                ],
+              }
+            ]
+          }
+        },
+        "End" : true
+      },
+      "Monthly reporting window" : {
+        "Type" : "Task",
+        "Comment" : "Reports Generator - creates a specific report needed for analysing GP2GP transfers",
+        "Resource" : "arn:aws:states:::ecs:runTask.sync",
+        "ResultPath" : null,
+        "Parameters" : {
+          "LaunchType" : "FARGATE",
+          "Cluster" : data.aws_ssm_parameter.data_pipeline_ecs_cluster_arn.value,
+          "TaskDefinition" : data.aws_ssm_parameter.reports_generator_task_definition_arn.value,
+          "NetworkConfiguration" : {
+            "AwsvpcConfiguration" : {
+              "Subnets" : [
+                data.aws_ssm_parameter.data_pipeline_private_subnet_id.value
+              ],
+              "SecurityGroups" : [
+                data.aws_ssm_parameter.outbound_only_security_group_id.value],
+            }
+          },
+          "Overrides" : {
+            "ContainerOverrides" : [
+              {
+                "Name" : "reports-generator",
+                "Environment" : [
+                  {
+                    "Name" : "REPORT_NAME",
+                    "Value.$" : "$.REPORT_NAME"
+                  },
+                  {
+                    "Name" : "CONVERSATION_CUTOFF_DAYS",
+                    "Value.$" : "$.CONVERSATION_CUTOFF_DAYS"
+                  },
+                  {
+                    "Name" : "NUMBER_OF_MONTHS",
+                    "Value.$" : "$.NUMBER_OF_MONTHS"
                   },
                 ],
               }
