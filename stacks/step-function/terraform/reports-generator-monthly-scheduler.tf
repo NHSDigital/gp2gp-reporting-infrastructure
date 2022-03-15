17237f8379242f51e1ce84f2fc5d03335afe36a9
Jake Newman <jacob.newman1@nhs.net>
2022-03-15 17:09:49 +0000

    PRMT-2543 [JN/HZ] Run ccg level integration times report on the 15th monthly

--- stacks/step-function/terraform/reports-generator-monthly-scheduler.tf
+++ stacks/step-function/terraform/reports-generator-monthly-scheduler.tf
@@ -21,3 +21,15 @@ resource "aws_cloudwatch_event_target" "monthly_transfer_outcomes_per_supplier_p
     "CONVERSATION_CUTOFF_DAYS" : "14",
     "NUMBER_OF_MONTHS" : "1" })
 }
+
+
+resource "aws_cloudwatch_event_target" "monthly_ccg_level_integration_times_report_event_trigger" {
+  target_id = "${var.environment}-monthly-reports-generator-ccg-level-integration-times-trigger"
+  rule      = aws_cloudwatch_event_rule.run_reports_once_a_month_cron_expression.name
+  arn       = aws_sfn_state_machine.reports_generator.arn
+  role_arn  = aws_iam_role.reports_generator_trigger.arn
+  input = jsonencode({
+    "REPORT_NAME" : "CCG_LEVEL_INTEGRATION_TIMES",
+    "CONVERSATION_CUTOFF_DAYS" : "14",
+    "NUMBER_OF_MONTHS" : "1" })
+}
