9d7d1735ec0f94bff2e199cdf53190824a71ad29
helen.zhou <helen.zhou1@nhs.net>
2022-03-17 17:11:23 +0000

    PRMT-2593 [JN/HZ] Add three transfer classifier tasks for cut off of 1, 2 and 14 days to step function

--- stacks/step-function/terraform/spine-exporter-and-transfer-classifier-step-function.tf
+++ stacks/step-function/terraform/spine-exporter-and-transfer-classifier-step-function.tf
@@ -25,19 +25,19 @@ resource "aws_sfn_state_machine" "spine_exporter_and_transfer_classifier" {
                 data.aws_ssm_parameter.data_pipeline_private_subnet_id.value
               ],
               "SecurityGroups" : [
-                data.aws_ssm_parameter.outbound_only_security_group_id.value],
+              data.aws_ssm_parameter.outbound_only_security_group_id.value],
             }
           }
         },
-        "Retry": [ {
-          "ErrorEquals": ["States.TaskFailed"],
-          "IntervalSeconds": 10,
-          "MaxAttempts": 3,
-          "BackoffRate": 2.0
-        } ],
-        "Next" : "TransferClassifier"
+        "Retry" : [{
+          "ErrorEquals" : ["States.TaskFailed"],
+          "IntervalSeconds" : 10,
+          "MaxAttempts" : 2,
+          "BackoffRate" : 2.0
+        }],
+        "Next" : "TransferClassifier1DayCutoff"
       },
-      "TransferClassifier" : {
+      "TransferClassifier1DayCutoff" : {
         "Type" : "Task",
         "Comment" : "Transfer Classifier - responsible for taking raw spine transfer data and organisation meta data and allocating transfers a status",
         "Resource" : "arn:aws:states:::ecs:runTask.sync",
@@ -62,7 +62,75 @@ resource "aws_sfn_state_machine" "spine_exporter_and_transfer_classifier" {
                 "Environment" : [
                   {
                     "Name" : "CONVERSATION_CUTOFF_DAYS",
-                    "Value.$" : "$.CONVERSATION_CUTOFF_DAYS"
+                    "Value" : "1"
+                  }
+                ],
+              }
+            ]
+          }
+        },
+        "Next" : "TransferClassifier2DayCutoff"
+      },
+      "TransferClassifier2DayCutoff" : {
+        "Type" : "Task",
+        "Comment" : "Transfer Classifier - responsible for taking raw spine transfer data and organisation meta data and allocating transfers a status",
+        "Resource" : "arn:aws:states:::ecs:runTask.sync",
+        "ResultPath" : null,
+        "Parameters" : {
+          "LaunchType" : "FARGATE",
+          "Cluster" : data.aws_ssm_parameter.data_pipeline_ecs_cluster_arn.value,
+          "TaskDefinition" : data.aws_ssm_parameter.transfer_classifier_task_definition_arn.value,
+          "NetworkConfiguration" : {
+            "AwsvpcConfiguration" : {
+              "Subnets" : [
+                data.aws_ssm_parameter.data_pipeline_private_subnet_id.value
+              ],
+              "SecurityGroups" : [
+              data.aws_ssm_parameter.outbound_only_security_group_id.value],
+            }
+          },
+          "Overrides" : {
+            "ContainerOverrides" : [
+              {
+                "Name" : "transfer-classifier",
+                "Environment" : [
+                  {
+                    "Name" : "CONVERSATION_CUTOFF_DAYS",
+                    "Value" : "2"
+                  }
+                ],
+              }
+            ]
+          }
+        },
+        "Next" : "TransferClassifier14DayCutoff"
+      },
+      "TransferClassifier14DayCutoff" : {
+        "Type" : "Task",
+        "Comment" : "Transfer Classifier - responsible for taking raw spine transfer data and organisation meta data and allocating transfers a status",
+        "Resource" : "arn:aws:states:::ecs:runTask.sync",
+        "ResultPath" : null,
+        "Parameters" : {
+          "LaunchType" : "FARGATE",
+          "Cluster" : data.aws_ssm_parameter.data_pipeline_ecs_cluster_arn.value,
+          "TaskDefinition" : data.aws_ssm_parameter.transfer_classifier_task_definition_arn.value,
+          "NetworkConfiguration" : {
+            "AwsvpcConfiguration" : {
+              "Subnets" : [
+                data.aws_ssm_parameter.data_pipeline_private_subnet_id.value
+              ],
+              "SecurityGroups" : [
+              data.aws_ssm_parameter.outbound_only_security_group_id.value],
+            }
+          },
+          "Overrides" : {
+            "ContainerOverrides" : [
+              {
+                "Name" : "transfer-classifier",
+                "Environment" : [
+                  {
+                    "Name" : "CONVERSATION_CUTOFF_DAYS",
+                    "Value" : "14"
                   }
                 ],
               }
