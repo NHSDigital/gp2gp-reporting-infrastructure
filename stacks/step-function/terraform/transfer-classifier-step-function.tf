3f63757b9a9ed5a40c9d09774f72b673a2d8bb8a
Jake Newman <jacob.newman1@nhs.net>
2022-03-18 10:54:08 +0000

    PRMT-2593 [JN/HZ] Clean up transfer-classifier step function to be used only for manual runs (remove choices)

--- stacks/step-function/terraform/transfer-classifier-step-function.tf
+++ stacks/step-function/terraform/transfer-classifier-step-function.tf
@@ -1,35 +1,16 @@
 resource "aws_sfn_state_machine" "transfer_classifier" {
-  name     = "transfer-classifier"
+  name     = "transfer-classifier-manual"
   role_arn = aws_iam_role.transfer_classifier_step_function.arn
   tags = merge(
     local.common_tags,
     {
-      Name = "Transfer Classifier Step Function"
+      Name = "Transfer Classifier Step Function Manual Run"
     }
   )
   definition = jsonencode({
-    "StartAt" : "Has Start and End Datetime",
+    "StartAt" : "TransferClassifier",
     "States" : {
-      "Has Start and End Datetime" : {
-        "Type" : "Choice",
-        "Choices" : [
-          {
-            "And" : [
-              {
-                "Variable" : "$.START_DATETIME",
-                "IsPresent" : true
-              },
-              {
-                "Variable" : "$.END_DATETIME",
-                "IsPresent" : true
-              }
-            ],
-            "Next" : "TransferClassifier (with start/end datetime)"
-          }
-        ],
-        "Default" : "TransferClassifier (without start/end datetime)"
-      },
-      "TransferClassifier (with start/end datetime)" : {
+      "TransferClassifier" : {
         "Type" : "Task",
         "Comment" : "Transfer Classifier - responsible for taking raw spine transfer data and organisation meta data and allocating transfers a status",
         "Resource" : "arn:aws:states:::ecs:runTask.sync",
@@ -75,44 +56,6 @@ resource "aws_sfn_state_machine" "transfer_classifier" {
         },
         "End" : true
       },
-      "TransferClassifier (without start/end datetime)" : {
-        "Type" : "Task",
-        "Comment" : "Transfer Classifier - responsible for taking raw spine transfer data and organisation meta data and allocating transfers a status",
-        "Resource" : "arn:aws:states:::ecs:runTask.sync",
-        "ResultPath" : null,
-        "Parameters" : {
-          "LaunchType" : "FARGATE",
-          "Cluster" : data.aws_ssm_parameter.data_pipeline_ecs_cluster_arn.value,
-          "TaskDefinition" : data.aws_ssm_parameter.transfer_classifier_task_definition_arn.value,
-          "NetworkConfiguration" : {
-            "AwsvpcConfiguration" : {
-              "Subnets" : [
-                data.aws_ssm_parameter.data_pipeline_private_subnet_id.value
-              ],
-              "SecurityGroups" : [
-              data.aws_ssm_parameter.outbound_only_security_group_id.value],
-            }
-          },
-          "Overrides" : {
-            "ContainerOverrides" : [
-              {
-                "Name" : "transfer-classifier",
-                "Environment" : [
-                  {
-                    "Name" : "OUTPUT_TRANSFER_DATA_BUCKET",
-                    "Value.$" : "$.OUTPUT_TRANSFER_DATA_BUCKET"
-                  },
-                  {
-                    "Name" : "CONVERSATION_CUTOFF_DAYS",
-                    "Value.$" : "$.CONVERSATION_CUTOFF_DAYS"
-                  }
-                ],
-              }
-            ]
-          }
-        },
-        "End" : true
-      }
     }
   })
 }
