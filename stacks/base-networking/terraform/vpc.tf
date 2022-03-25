94a843fd16b99ac6e1047221649e0506e3a9f1ae
helen.zhou <helen.zhou1@nhs.net>
2022-03-25 15:47:40 +0000

    PRMT-2605 [JN/HZ] Change flow log terraform to point to log destination

--- stacks/base-networking/terraform/vpc.tf
+++ stacks/base-networking/terraform/vpc.tf
@@ -30,10 +30,10 @@ locals {
 }
 
 resource "aws_flow_log" "vpc_flow_log" {
-  iam_role_arn         = aws_iam_role.vpc_flow_log.arn
-  log_destination_type = aws_cloudwatch_log_group.vpc_flow_log.arn
-  traffic_type         = "ALL"
-  vpc_id               = aws_vpc.vpc.id
+  iam_role_arn    = aws_iam_role.vpc_flow_log.arn
+  log_destination = aws_cloudwatch_log_group.vpc_flow_log.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.vpc.id
 }
 
 resource "aws_cloudwatch_log_group" "vpc_flow_log" {
