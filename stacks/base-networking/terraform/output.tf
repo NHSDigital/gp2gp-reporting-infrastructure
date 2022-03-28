576a1ee3a653324a2ffabadc1b7eea36a60d4c78
helen.zhou <helen.zhou1@nhs.net>
2022-03-28 14:44:50 +0100

    PRMT-2605 [JN/HZ] Create security group associated with outbound requests from GoCD VPC

--- stacks/base-networking/terraform/output.tf
+++ stacks/base-networking/terraform/output.tf
@@ -11,3 +11,10 @@ resource "aws_ssm_parameter" "outbound_only_security_group_id" {
   value = aws_security_group.outbound_only.id
   tags  = local.common_tags
 }
+
+resource "aws_ssm_parameter" "gocd_vpc_outbound_only_security_group_id" {
+  name  = "/registrations/${var.environment}/data-pipeline/base-networking/gocd-vpc-outbound-only-security-group-id"
+  type  = "String"
+  value = aws_security_group.gocd_vpc_outbound_only.id
+  tags  = local.common_tags
+}
--- stacks/base-networking/terraform/security-group.tf
+++ stacks/base-networking/terraform/security-group.tf
@@ -19,3 +19,25 @@ resource "aws_security_group_rule" "outbound_only" {
   protocol          = "-1"
   description       = "Unrestricted egress"
 }
+
+resource "aws_security_group" "gocd_vpc_outbound_only" {
+  name   = "${var.environment}-gocd-vpc-outbound-only"
+  vpc_id = data.aws_ssm_parameter.gocd_vpc_id.value
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.environment}-data-pipeline-outbound-only"
+    }
+  )
+}
+
+resource "aws_security_group_rule" "gocd_vpc_outbound_only" {
+  type              = "egress"
+  security_group_id = aws_security_group.gocd_vpc_outbound_only.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  description       = "Unrestricted egress"
+}
