9e593bf26995cd88883c79b04bfce381e814502e
Jake Newman <jacob.newman1@nhs.net>
2022-04-07 16:46:05 +0100

    PRMT-2563 [JN] Add spine-exporter cloudwatch dashboard to show error and stats

--- stacks/spine-exporter/terraform/monitoring.tf
+++ stacks/spine-exporter/terraform/monitoring.tf
@@ -9,8 +9,52 @@ resource "aws_cloudwatch_dashboard" "data_pipeline" {
         "height" : 6,
         "properties" : {
           "region" : var.region,
-          "title" : "Count of errors grouped by error type and hour",
-          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.name}' | fields @timestamp, error | filter ispresent(error) | stats count(*) as totalCount by error, bin (1h) as timeframe",
+          "title" : "SPINE_EXTRACT_ROW_COUNT",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | stats sum(row_count) by bin(1h) | filter strcontains(@logStream, 'spine-exporter') and event='SPINE_EXTRACT_ROW_COUNT'",
+          "view" : "table"
+        }
+      },
+      {
+        "type" : "log",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "region" : var.region,
+          "title" : "Details error messages",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | fields @timestamp, event, message, @message | filter strcontains(@logStream, 'spine-exporter') and level != 'INFO'",
+          "view" : "table"
+        }
+      },
+      {
+        "type" : "log",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "region" : var.region,
+          "title" : "SPINE_EXTRACT_SIZE_BYTES",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | stats sum(size_in_bytes) by bin(1h) | filter strcontains(@logStream, 'spine-exporter') and event='SPINE_EXTRACT_SIZE_BYTES'",
+          "view" : "table"
+        }
+      },
+      {
+        "type" : "log",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "region" : var.region,
+          "title" : "SPINE_EXTRACT_SIZE_BYTES",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | stats count(event) by bin(1d) | filter strcontains(@logStream, 'spine-exporter') and event='UPLOADED_CSV_TO_S3'",
+          "view" : "table"
+        }
+      },
+      {
+        "type" : "log",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "region" : var.region,
+          "title" : "SPINE_EXTRACT_SIZE_BYTES",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | stats count(event) by bin(1d) | filter strcontains(@logStream, 'spine-exporter') and event='FAILED_TO_RUN_MAIN'",
           "view" : "table"
         }
       },
