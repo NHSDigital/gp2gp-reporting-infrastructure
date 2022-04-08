b88c9268dddc55682a39d4faa210ff67ef24b97b
Jake Newman <jacob.newman1@nhs.net>
2022-04-08 19:28:51 +0100

    PRMT-2563 [JN] Update dashboard for spine-exporter with counts, graphs and all logs

--- stacks/spine-exporter/terraform/monitoring.tf
+++ stacks/spine-exporter/terraform/monitoring.tf
@@ -23,7 +23,7 @@ resource "aws_cloudwatch_dashboard" "data_pipeline" {
           "period" : 120
           "region" : var.region,
           "title" : "SPINE_EXTRACT_SIZE_BYTES - graph",
-          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | stats sum(soze_in_bytes) by bin(1h) | filter strcontains(@logStream, 'spine-exporter') and event='SPINE_EXTRACT_SIZE_BYTES'",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | stats sum(size_in_bytes) by bin(1h) | filter strcontains(@logStream, 'spine-exporter') and event='SPINE_EXTRACT_SIZE_BYTES'",
           "view" : "timeSeries"
         }
       },
@@ -47,7 +47,7 @@ resource "aws_cloudwatch_dashboard" "data_pipeline" {
           "period" : 120
           "region" : var.region,
           "title" : "SPINE_EXTRACT_ROW_COUNT - graph",
-          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | stats sum(size_in_bytes) by bin(1d) | filter strcontains(@logStream, 'spine-exporter') and event='SPINE_EXTRACT_ROW_COUNT'",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | stats sum(row_count) by bin(1d) | filter strcontains(@logStream, 'spine-exporter') and event='SPINE_EXTRACT_ROW_COUNT'",
           "view" : "timeSeries"
         }
       },
@@ -82,18 +82,6 @@ resource "aws_cloudwatch_dashboard" "data_pipeline" {
         "properties" : {
           "period" : 120
           "region" : var.region,
-          "title" : "Successful upload count - test source",
-          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' |  stats count(event) as count by bin(1d) as timestamp | filter strcontains(@logStream, 'spine-exporter') event='UPLOADED_CSV_TO_S3'",
-          "view" : "timeSeries",
-        }
-      },
-      {
-        "type" : "log",
-        "width" : 12,
-        "height" : 6,
-        "properties" : {
-          "period" : 120
-          "region" : var.region,
           "title" : "FAILED_TO_RUN_MAIN",
           "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | stats count(event) as count by bin(1d) as timestamp | filter strcontains(@logStream, 'spine-exporter') and event='FAILED_TO_RUN_MAIN'",
           "view" : "table"
@@ -109,6 +97,18 @@ resource "aws_cloudwatch_dashboard" "data_pipeline" {
           "title" : "Detailed error messages",
           "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | fields @timestamp, event, message, @message | filter strcontains(@logStream, 'spine-exporter') and level != 'INFO'",
           "view" : "table"
+        }
+      },
+      {
+        "type" : "log",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "period" : 120
+          "region" : var.region,
+          "title" : "All log messages",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | fields @timestamp, message, @message | filter strcontains(@logStream, 'spine-exporter')",
+          "view" : "table",
         }
       },
     ]
