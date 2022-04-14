69942b818809343274a2fe79d28cbfd367a17778
Jake Newman <jacob.newman1@nhs.net>
2022-04-14 11:48:41 +0100

    PRMT-2563 [JN] Add dashboard for reports generator

---
+++ stacks/reports-generator/terraform/monitoring.tf
@@ -0,0 +1,80 @@
+resource "aws_cloudwatch_dashboard" "data_pipeline" {
+  dashboard_name = "${var.environment}-registrations-data-pipeline-reports-generator"
+  dashboard_body = jsonencode({
+    "start" : "-P3D"
+    "widgets" : [
+      {
+        "type" : "log",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "period" : 120
+          "region" : data.aws_region.current.name,
+          "title" : "Produced reports",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | fields @timestamp, @message | filter strcontains(@logStream, 'reports-generator') and event='PRODUCED_*'",
+          "view" : "table",
+        }
+      },
+      {
+        "type" : "log",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "period" : 120
+          "region" : data.aws_region.current.name,
+          "title" : "Successful upload count",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' |  stats count(event) as count by bin(1d) as timestamp | filter strcontains(@logStream, 'reports-generator') and event='UPLOADED_JSON_TO_S3'",
+          "view" : "table",
+        }
+      },
+      {
+        "type" : "log",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "period" : 120
+          "region" : data.aws_region.current.name,
+          "title" : "Successful upload count - graph",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' |  stats count(event) as count by bin(1d) as timestamp | sort timestamp | filter strcontains(@logStream, 'reports-generator') and event='UPLOADED_JSON_TO_S3'",
+          "view" : "bar",
+        }
+      },
+      {
+        "type" : "log",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "period" : 120
+          "region" : data.aws_region.current.name,
+          "title" : "FAILED_TO_RUN_MAIN",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | stats count(event) as count by bin(1d) as timestamp  | filter strcontains(@logStream, 'reports-generator') and event='FAILED_TO_RUN_MAIN'",
+          "view" : "table"
+        }
+      },
+      {
+        "type" : "log",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "period" : 120
+          "region" : data.aws_region.current.name,
+          "title" : "Non-info logs (errors, warnings, system)",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | fields @timestamp, event, message, @message | filter strcontains(@logStream, 'reports-generator') and level != 'INFO'",
+          "view" : "table"
+        }
+      },
+      {
+        "type" : "log",
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "period" : 120
+          "region" : data.aws_region.current.name,
+          "title" : "All log messages",
+          "query" : "SOURCE '${data.aws_ssm_parameter.cloud_watch_log_group.value}' | fields @timestamp, message, @message | filter strcontains(@logStream, 'reports-generator')",
+          "view" : "table",
+        }
+      },
+    ]
+  })
+}
