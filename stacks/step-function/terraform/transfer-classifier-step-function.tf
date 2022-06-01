0c8d2cc125faa901c923b3ec0901cf6f543dd14b
Jake Newman <jacob.newman1@nhs.net>
2022-06-01 10:27:12 +0100

    PRMT-2653 [JN] Support classifying MI events with transfer-classifier step function

--- stacks/step-function/terraform/transfer-classifier-step-function.tf
+++ stacks/step-function/terraform/transfer-classifier-step-function.tf
@@ -10,9 +10,70 @@ resource "aws_sfn_state_machine" "transfer_classifier" {
   definition = jsonencode({
     "StartAt" : "TransferClassifier",
     "States" : {
-      "TransferClassifier" : {
+      "Classify Spine or MiEvents?" : {
+        "Type" : "Choice",
+        "Choices" : [
+          {
+            "Variable" : "$.CLASSIFY_MI_EVENTS",
+            "BooleanEquals" : true,
+            "Next" : "TransferClassifierMiEvents"
+          }
+        ],
+        "Default" : "TransferClassifierSpine"
+      },
+      "TransferClassifierMiEvents" : {
         "Type" : "Task",
-        "Comment" : "Transfer Classifier - responsible for taking raw spine transfer data and organisation meta data and allocating transfers a status",
+        "Comment" : "Transfer Classifier Mi Events - responsible for taking mi events and organisation meta data and allocating transfers a status",
+        "Resource" : "arn:aws:states:::ecs:runTask.sync",
+        "ResultPath" : null,
+        "Parameters" : {
+          "LaunchType" : "FARGATE",
+          "Cluster" : data.aws_ssm_parameter.data_pipeline_ecs_cluster_arn.value,
+          "TaskDefinition" : data.aws_ssm_parameter.transfer_classifier_task_definition_arn.value,
+          "NetworkConfiguration" : {
+            "AwsvpcConfiguration" : {
+              "Subnets" : [
+                data.aws_ssm_parameter.data_pipeline_private_subnet_id.value
+              ],
+              "SecurityGroups" : [
+                data.aws_ssm_parameter.outbound_only_security_group_id.value],
+            }
+          },
+          "Overrides" : {
+            "ContainerOverrides" : [
+              {
+                "Name" : "transfer-classifier",
+                "Environment" : [
+                  {
+                    "Name" : "START_DATETIME",
+                    "Value.$" : "$.START_DATETIME"
+                  },
+                  {
+                    "Name" : "END_DATETIME",
+                    "Value.$" : "$.END_DATETIME"
+                  },
+                  {
+                    "Name" : "OUTPUT_TRANSFER_DATA_BUCKET",
+                    "Value.$" : "$.OUTPUT_TRANSFER_DATA_BUCKET"
+                  },
+                  {
+                    "Name" : "CONVERSATION_CUTOFF_DAYS",
+                    "Value.$" : "$.CONVERSATION_CUTOFF_DAYS"
+                  },
+                  {
+                    "Name" : "CLASSIFY_MI_EVENTS",
+                    "Value" : "True"
+                  }
+                ],
+              }
+            ]
+          }
+        },
+        "End" : true
+      },
+      "TransferClassifierSpine" : {
+        "Type" : "Task",
+        "Comment" : "Transfer Classifier Spine - responsible for taking raw spine transfer data and organisation meta data and allocating transfers a status",
         "Resource" : "arn:aws:states:::ecs:runTask.sync",
         "ResultPath" : null,
         "Parameters" : {
