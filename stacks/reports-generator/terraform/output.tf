f97d4d14ba749071dd8090e48c8b397dc3dd9734
Jake Newman <jacob.newman1@nhs.net>
2022-08-17 15:40:49 +0100

    PRMT-2643 [JN] Output reports generator bucket name as ssm param for email lambda to use

--- stacks/reports-generator/terraform/output.tf
+++ stacks/reports-generator/terraform/output.tf
@@ -11,3 +11,10 @@ resource "aws_ssm_parameter" "reports_generator_iam_role_arn" {
   value = aws_iam_role.reports_generator.arn
   tags  = local.common_tags
 }
+
+resource "aws_ssm_parameter" "reports_generator_output_bucket_name" {
+  name  = "/registrations/${var.environment}/data-pipeline/reports-generator/output-bucket-name"
+  type  = "String"
+  value = aws_s3_bucket.reports_generator.bucket
+  tags  = local.common_tags
+}
