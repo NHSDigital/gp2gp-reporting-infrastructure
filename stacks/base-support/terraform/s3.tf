aa5176bc6cd476d8d1a34c2d57da967a38764dd2
Jake Newman <jacob.newman1@nhs.net>
2022-11-18 18:31:39 +0000

    PRMT-2952 [JN] Update config to point to old, temporary terraform state bucket to fix db before s3. Comment out new s3 terraform state

--- stacks/base-support/terraform/s3.tf
+++ stacks/base-support/terraform/s3.tf
@@ -1,45 +1,45 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.s3_terraform_state_bucket_name
-  acl    = "private"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  # To cleanup old states eventually
-  lifecycle_rule {
-    enabled = true
-
-    noncurrent_version_expiration {
-      days = 360
-    }
-  }
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "Terraform states of GP2GP infrastructure"
-    }
-  )
-}
-
-resource "aws_s3_bucket_acl" "metrics_calculator" {
-  bucket = aws_s3_bucket.terraform_state.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "reports_generator" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "terraform_state_block" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#resource "aws_s3_bucket" "terraform_state" {
+#  bucket = var.s3_terraform_state_bucket_name
+#  acl    = "private"
+#
+#  lifecycle {
+#    prevent_destroy = true
+#  }
+#
+#  # To cleanup old states eventually
+#  lifecycle_rule {
+#    enabled = true
+#
+#    noncurrent_version_expiration {
+#      days = 360
+#    }
+#  }
+#
+#  tags = merge(
+#    local.common_tags,
+#    {
+#      Name = "Terraform states of GP2GP infrastructure"
+#    }
+#  )
+#}
+#
+#resource "aws_s3_bucket_acl" "metrics_calculator" {
+#  bucket = aws_s3_bucket.terraform_state.id
+#  acl    = "private"
+#}
+#
+#resource "aws_s3_bucket_versioning" "reports_generator" {
+#  bucket = aws_s3_bucket.terraform_state.id
+#  versioning_configuration {
+#    status = "Enabled"
+#  }
+#}
+#
+#resource "aws_s3_bucket_public_access_block" "terraform_state_block" {
+#  bucket = aws_s3_bucket.terraform_state.id
+#
+#  block_public_acls       = true
+#  block_public_policy     = true
+#  ignore_public_acls      = true
+#  restrict_public_buckets = true
+#}
