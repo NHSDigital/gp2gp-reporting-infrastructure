304aca869d12facc95572f37c99d9255a1547d5e
Jake Newman <jacob.newman1@nhs.net>
2022-11-23 09:31:10 +0000

    PRMT-2952 [JN] Try to use correct security group after temporarily associating with a different group to allow changes

--- stacks/base-networking/terraform/security-group.tf
+++ stacks/base-networking/terraform/security-group.tf
@@ -45,26 +45,3 @@ resource "aws_security_group_rule" "gocd_vpc_outbound_only" {
   protocol          = "-1"
   description       = "Unrestricted egress"
 }
-
-# TODO : delete after setup prod
-resource "aws_security_group" "gocd_vpc_outbound_only_preprod_temp" {
-  name   = "preprod-gocd-vpc-outbound-only"
-  vpc_id = data.aws_ssm_parameter.gocd_vpc_id.value
-
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "preprod-gocd-vpc-outbound-only"
-    }
-  )
-}
-
-resource "aws_security_group_rule" "gocd_vpc_outbound_only_preprood_temp" {
-  type              = "egress"
-  security_group_id = aws_security_group.gocd_vpc_outbound_only_preprod_temp.id
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  description       = "Unrestricted egress"
-}
