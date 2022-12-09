d83b3657b979a4f6c7604929990163fe8a6b9c1b
Jake Newman <jacob.newman1@nhs.net>
2022-12-09 10:43:05 +0000

    PRMT-2997 [JN/JP] Add envtype tag to resources to conform to CSOC requirements

--- stacks/spine-exporter/terraform/tags.tf
+++ stacks/spine-exporter/terraform/tags.tf
@@ -2,6 +2,8 @@ locals {
   common_tags = {
     CreatedBy   = var.repo_name
     Environment = var.environment
+    envtype = var.environment
+    envtype = var.environment
     Team        = var.team
   }
 }
