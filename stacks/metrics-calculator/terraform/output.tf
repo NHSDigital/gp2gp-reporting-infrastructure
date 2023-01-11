0a6e9e37d67d6198413126c764a0851b1b2a2f48
Jake Newman <jacob.newman1@nhs.net>
2023-01-11 15:39:33 +0000

    PRMT-1437 [JN/JP] Store metrics_calculator s3 bucket as an ssm param to be used in the gp2gp dashboard

--- stacks/metrics-calculator/terraform/output.tf
+++ stacks/metrics-calculator/terraform/output.tf
@@ -23,3 +23,16 @@ resource "aws_ssm_parameter" "metrics_calculator_iam_role_arn" {
     }
   )
 }
+
+resource "aws_ssm_parameter" "metrics_calculator_output_bucket_name" {
+  name  = "/registrations/${var.environment}/data-pipeline/metrics-calculator/output-bucket-name"
+  type  = "String"
+  value = aws_s3_bucket.metrics_calculator.bucket
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.environment}-metrics-calculator-output-bucket-name"
+      ApplicationRole = "AwsSsmParameter"
+    }
+  )
+}
