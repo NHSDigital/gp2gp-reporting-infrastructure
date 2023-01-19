d5f030f3cd0af0cee7912c2910c1dfdb0bb26774
jowita <jowita.podolak1@nhs.net>
2023-01-19 12:01:05 +0000

    PRMT-3084 [JP/JN] Add logging to validate metrics lambda with a new logging group

--- stacks/validate-metrics/terraform/iam.tf
+++ stacks/validate-metrics/terraform/iam.tf
@@ -3,24 +3,13 @@ resource "aws_iam_role" "validate_metrics_lambda_role" {
   assume_role_policy = data.aws_iam_policy_document.validate_metrics_lambda_assume_role.json
   managed_policy_arns = [
     aws_iam_policy.validate_metrics_lambda_ssm_access.arn,
-#    aws_iam_policy.validate_metrics_cloudwatch_log_access.arn,
-#    aws_iam_policy.metrics_input_bucket_read_access.arn,
+    aws_iam_policy.validate_metrics_cloudwatch_log_access.arn,
   ]
 }
 
 resource "aws_iam_policy" "validate_metrics_lambda_ssm_access" {
   name   = "${var.environment}-validate-metrics-ssm--access"
   policy = data.aws_iam_policy_document.validate_metrics_lambda_ssm_access.json
-}
-
-data "aws_iam_policy_document" "validate_metrics_lambda_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
 }
 
 data "aws_iam_policy_document" "validate_metrics_lambda_ssm_access" {
@@ -35,5 +24,46 @@ data "aws_iam_policy_document" "validate_metrics_lambda_ssm_access" {
       "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter${var.s3_national_metrics_filepath_param_name}",
       "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter${var.s3_practice_metrics_filepath_param_name}",
     ]
+  }
+}
+
+resource "aws_iam_policy" "validate_metrics_cloudwatch_log_access" {
+  name   = "${var.environment}-validate-metrics-cloudwatch-log--access"
+  policy = data.aws_iam_policy_document.validate_metrics-cloudwatch-log-access.json
+}
+
+
+data "aws_iam_policy_document" "validate_metrics-cloudwatch-log-access" {
+  statement {
+    sid = "CloudwatchLogs"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "${aws_cloudwatch_log_group.validate_metrics_logs_group.arn}:*",
+    ]
+  }
+}
+
+resource "aws_cloudwatch_log_group" "validate_metrics_logs_group" {
+  name = "/aws/lambda/${var.environment}-${var.validate_metrics_lambda_name}"
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.environment}-${var.validate_metrics_lambda_name}"
+      ApplicationRole = "AwsCloudwatchLogGroup"
+    }
+  )
+  retention_in_days = 60
+}
+
+data "aws_iam_policy_document" "validate_metrics_lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
   }
 }
