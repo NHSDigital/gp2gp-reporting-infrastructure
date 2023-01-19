1dc1aecbd11081ba4844bef870b668b7f4a2dcf2
jowita <jowita.podolak1@nhs.net>
2023-01-19 15:25:24 +0000

    PRMT-3084 [JP/JN] Output validate metrics lambda arn as an ssm parameter

---
+++ stacks/validate-metrics/terraform/output.tf
@@ -0,0 +1,12 @@
+resource "aws_ssm_parameter" "validate_metrics_lambda_arn" {
+  name  = "/registrations/${var.environment}/data-pipeline/validate-metrics/lambda-arn"
+  type  = "String"
+  value = aws_lambda_function.validate_metrics_lambda.arn
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.environment}-validate-metrics-lambda-definition-arn"
+      ApplicationRole = "AwsSsmParameter"
+    }
+  )
+}
