f0582e8cf6b769f3532abf2f32c123d9eb38d733
Jake Newman <jacob.newman1@nhs.net>
2023-01-20 11:24:17 +0000

    PRMT-1437 [JP/JN] Skip metrics option (choice) in gp2gp dashboard step function to avoid generating/validating metrics every time [useful for dashboard code changes]

--- stacks/step-function/terraform/dashboard-pipeline-event-trigger.tf
+++ stacks/step-function/terraform/dashboard-pipeline-event-trigger.tf
@@ -3,4 +3,8 @@ resource "aws_cloudwatch_event_target" "monthly_dashboard_pipeline_event_trigger
   rule      = aws_cloudwatch_event_rule.run_once_a_month_on_15th_cron_expression.name
   arn       = aws_sfn_state_machine.dashboard_pipeline.arn
   role_arn  = aws_iam_role.dashboard_pipeline_trigger.arn
+
+  input = jsonencode({
+    "SKIP_METRICS" : false,
+  })
 }
