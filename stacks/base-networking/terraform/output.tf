cf0b49ab0ec2453bd4f2abbbf23288d27e01ae9f
Jake Newman <jacob.newman1@nhs.net>
2023-02-08 11:58:24 +0000

    PRMT-1437 [JN] Remove unused output for gocd_vpc_outbound_only_security_group_id as gocd trigger is removed

--- stacks/base-networking/terraform/output.tf
+++ stacks/base-networking/terraform/output.tf
@@ -25,15 +25,3 @@ resource "aws_ssm_parameter" "outbound_only_security_group_id" {
   )
 }
 
-resource "aws_ssm_parameter" "gocd_vpc_outbound_only_security_group_id" {
-  name  = "/registrations/${var.environment}/data-pipeline/base-networking/gocd-vpc-outbound-only-security-group-id"
-  type  = "String"
-  value = aws_security_group.gocd_vpc_outbound_only.id
-  tags  = merge(
-    local.common_tags,
-    {
-      Name            = "${var.environment}-gocd-vpc-outbound-only-security-group-id"
-      ApplicationRole = "AwsSsmParameter"
-    }
-  )
-}
