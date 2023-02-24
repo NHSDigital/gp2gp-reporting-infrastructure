d5cf4680e150a002a0f5beef91717a97de3f23f6
Jake Newman <jacob.newman1@nhs.net>
2023-02-24 11:28:26 +0000

    PRMT-3085 [JN] Update output of gp2gp_dashboard_alert_lambda_arn

--- stacks/email-and-alerting/terraform/output.tf
+++ stacks/email-and-alerting/terraform/output.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "gp2gp_dashboard_alert_lambda_arn" {
-  name  = "/registrations/${var.environment}/data-pipeline/gp2gp-dashboard/gp2gp-dashboard-alert-lambda-arn"
+  name  = "/registrations/${var.environment}/data-pipeline/email-and-alerting/gp2gp-dashboard-alert-lambda-arn"
   type  = "String"
   value = aws_lambda_function.gp2gp_dashboard_alert_lambda.arn
   tags = merge(
