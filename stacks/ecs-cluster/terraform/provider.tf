c235ab7af0c528703858259c326a7a424cee4d72
JoeSykesNHS <joe.sykes1@nhs.net>
2023-04-04 15:25:10 +0100

    PRMT-1892 [JS] update provider.tf file in stacks to conform to latest terraform standards

--- stacks/ecs-cluster/terraform/provider.tf
+++ stacks/ecs-cluster/terraform/provider.tf
@@ -1,4 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.74"
+    }
+  }
+}
+
 provider "aws" {
   region = "eu-west-2"
-  version = "~> 3.74"
 }
--- stacks/gp2gp-dashboard/terraform/provider.tf
+++ stacks/gp2gp-dashboard/terraform/provider.tf
@@ -1,10 +1,17 @@
 provider "aws" {
   region = var.region
-  version = "~> 3.74"
 }
 
 provider "aws" {
   alias  = "cf_certificate_only_region"
   region = "us-east-1"
-  version = "~> 3.74"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.74"
+    }
+  }
 }
