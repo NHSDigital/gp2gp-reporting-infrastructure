cba9e8b57f48a6a641be98f78f35985323fcba90
Jack Sutton <jack.sutton@madetech.com>
2024-10-22 11:02:33 +0100

    formats terraform and uses self references and variables

--- stacks/email-and-alerting/terraform/ses.tf
+++ stacks/email-and-alerting/terraform/ses.tf
@@ -2,12 +2,12 @@ resource "aws_ses_email_identity" "email_report" {
   email = data.aws_ssm_parameter.email_report_sender_email.value
 }
 
-resource "aws_s3_bucket" "gp2gp_email_storage"{
-  bucket = "gp2gp_email_storage"
+resource "aws_s3_bucket" "gp2gp-inbox"{
+  bucket = "${var.environment}-gp2gp-inbox"
 }
 
-resource "aws_ses_domain_identity" "gp2gp_email_inbox" {
-  domain = "mail.dev.gp-registrations-data.nhs.uk"
+resource "aws_ses_domain_identity" "gp2gp-inbox" {
+  domain = "mail.${var.environment}.gp-registrations-data.nhs.uk"
 }
 
 resource "aws_ses_receipt_rule_set" "asid_lookup" {
@@ -16,16 +16,16 @@ resource "aws_ses_receipt_rule_set" "asid_lookup" {
 
 resource "aws_ses_receipt_rule" "email_store" {
   name          = "email_store"
-  rule_set_name = "default-rule-set"
+  rule_set_name = aws_ses_receipt_rule_set.asid_lookup.rule_set_name
   enabled       = true
   scan_enabled  = true
   s3_action {
-    bucket_name = "gp2gp_email_storage"
+    bucket_name = aws_s3_bucket.gp2gp_email_storage.id
     position    = 1
   }
 }
 
-data "aws_iam_policy_document" "example" {
+data "aws_iam_policy_document" "SES-store-emails" {
   statement  {
       "Sid": "AllowSESPuts",
       "Effect": "Allow",
@@ -33,7 +33,7 @@ data "aws_iam_policy_document" "example" {
           "Service": "ses.amazonaws.com"
       },
       "Action": "s3:PutObject",
-      "Resource": "arn:aws:s3:::gp2gp-email-storage/*",
+      "Resource": aws_s3_bucket.,
       "Condition": {
           "StringEquals": {
               "AWS:SourceAccount": local.account_id,
