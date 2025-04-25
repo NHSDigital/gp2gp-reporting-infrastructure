cce6a8854d8cd88457f50763032e09a67d60335e
AndyFlintNHS <andrew.flint2@nhs.net>
2025-04-25 10:25:21 +0100

    [PRM-153] Added store-asid-lookup lambda

--- lambda/store-asid-lookup/main.py
+++ lambda/store-asid-lookup/main.py
@@ -1,159 +1,140 @@
 import boto3
+import gzip
+import email
+import os
+from datetime import datetime, timezone
+from io import BytesIO
+from botocore.exceptions import ClientError
 
 s3_client = boto3.client('s3')
-client = boto3.client('secretsmanager', region_name='eu-west-2')
 ssm_client = boto3.client('ssm')
-environment =  os.environ["ENVIRONMENT"]
+
+# Constants
+environment = os.environ["ENVIRONMENT"]
+expected_destination = f"asidlookup@mail{environment}.gp-registrations-data.nhs.uk"
+permitted_emails_ssm_location = f"/registrations/{environment}/data-pipeline/gp2gp-dashboard/permitted-emails"
+source_s3_bucket_ssm_location = f'/registrations/{environment}/data-pipeline/gp2gp-dashboard/email-storage-bucket-name'
+asid_lookup_filename = "asidLookup.csv"
+destination_s3_bucket = f"prm-gp2gp-asid-lookup-{environment}"
 
 
 def lambda_handler(event, context):
-    # TODO PRMP-1059
-    #  take event -> get Records[0] from it
-    #  validate records[0] matches the schema in the ticket
-    #       if it doesn't, delete it from S3
-    #  Extract the attachment (asidLookup.csv).
-    #  gzip the file.
-    #  Put in file in S3 (s3://prm-gp2gp-asid-lookup-{env}/<YYYY>/<(M)M>/asidLookup.csv.gz).
-
     email_event = event['Records'][0]
 
-    validate_event(email_event) # TODO PRMP-1059 catch exception in this scope and trigger delete logic?
+    message_id = email_event['ses']['mail']['messageId']
+    print(f"Processing email with messageId: {message_id}")
 
-    attached_csv = extract_csv_attachment_from_email(email_event)
-
+    validate_email_event(email_event)
+    raw_email = get_raw_email_from_source_s3(email_event)
+    attached_csv = extract_csv_attachment_from_email(raw_email)
     compressed_csv = compress_csv(attached_csv)
+    store_file_in_destination_s3(compressed_csv)
 
-    store_file_in_s3(compressed_csv)
-
-
-def validate_event(email_event: dict):
-    permitted_email_addresses = get_permitted_emails()
-    # =============EXPECTED SCHEMA================
-    # ses.mail.source = .*=<allowed value from SSM Parameter>$
-    # ses.mail.destination[0] contains asidlookup@mail{env_value}.gp-registrations-data.nhs.uk
-    # ses.mail.headers[0].X-SES-Spam-Verdict = PASS
-    # ses.mail.headers[0].X-SES-Virus-Verdict = PASS
-    # ses.mail.headers[0].X-MS-Has-Attach ???
-    # ses.receipt.spamVerdict.status = PASS
-    # ses.receipt.virusVerdict.status = PASS
-    # ses.receipt.spfVerdict.status = PASS ???
-    # ses.receipt.dkimVerdict.status = PASS ???
-
-    schema = {
-        "ses": {
-            "type": "object",
-            "properties": {
-                "mail": {
-                    "type": "object",
-                    "properties": {
-                        "source": {
-                            "type": "string",
-                            "enum": permitted_email_addresses,
-                        "destination": {
-                            "type": "array",
-                            "items": {
-                                "type": "string",
-                                "pattern": fr"^.*asidlookup@mail{environment}\.gp-registrations-data\.nhs\.uk$"
-                            }
-                        },
-                        "headers": {
-                            "type": "array",
-                            "items": {
-                                "type": "object",
-                                "properties": {
-                                    "X-SES-Spam-Verdict": {"type": "string", "const": "PASS"},
-                                    "X-SES-Virus-Verdict": {"type": "string", "const": "PASS"},
-                                    "X-MS-Has-Attach": {"type": "string"} # TODO PRMP-1059 establish what this can be
-                                },
-                            },
-                            "minItems": 1
-                        }
-                    },
-                    "required": ["source", "destination", "headers"]
-                },
-                "receipt": {
-                    "type": "object",
-                    "properties": {
-                        "spamVerdict": {
-                            "type": "object",
-                            "properties": {
-                                "status": {
-                                    "type": "string",
-                                    "const": "PASS"
-                                }
-                            }
-                        },
-                        "virusVerdict": {
-                            "type": "object",
-                            "properties": {
-                                "status": {
-                                    "type": "string",
-                                    "const": "PASS"
-                                }
-                            }
-                        },
-                        "spfVerdict": {
-                            "type": "object",
-                            "properties": {
-                                "status": {
-                                    "type": "string",
-                                    "const": "PASS"
-                                }
-                            }
-                        },
-                        "dkimVerdict": {
-                            "type": "object",
-                            "properties": {
-                                "status": {
-                                    "type": "string",
-                                    "const": "PASS"
-                                }
-                            }
-                        }
-                    },
-                    "required": ["spamVerdict", "virusVerdict", "spfVerdict", "dkimVerdict"]
-                }
-            },
-            "required": ["mail", "receipt"]
-        }
-    },
-        "required": ["ses"]
-    }
-
-    email_event_as_json = json.dumps(email_event)
+def validate_email_event(email_event: dict):
+    ses_mail = email_event['ses']['mail']
+    ses_receipt = email_event['ses']['receipt']
 
     try:
-        validate(instance = email_event_as_json, schema = schema)
-        # TODO PRMP-1059 log something here indicating success
-    except ValidationError as e:
-        # TODO PRMP-1059 log something here indicating failure
+        validate_event_source(ses_mail)
+        validate_event_destination(ses_mail)
+        validate_event_headers(ses_mail)
+        validate_event_receipt(ses_receipt)
 
-def get_permitted_emails():
-    ssm_response = ssm_client.get_parameters(f'/registrations/{environment}/data-pipeline/gp2gp-dashboard/permitted-emails')
-    return ssm_response.parameter.value
+        print("Email event validated successfully")
+    except Exception as e:
+        remove_email_from_s3(email_event)
+        raise e
 
-def get_bucket_name():
-   bucket_name = ssm_client.get_parameters(f'/registrations/{environment}/data-pipeline/gp2gp-dashboard/email-storage-bucket-name')
-   return bucket_name
+def validate_event_source(ses_mail: dict):
+    raw_permitted_emails = get_ssm_param(permitted_emails_ssm_location) # permitted emails should be comma separated
+    permitted_emails = [email_address.strip() for email_address in raw_permitted_emails.split(",")]
 
-def extract_csv_attachment_from_email(email_event):
-    '''
-    We're not certain what the name of the CSV file is on S3.
-    It'll live in prm-gp2gp-inbox-storage-<env>/asid_lookup
-    however we're not sure how to marry up SES events -> S3 objects
+    source = ses_mail['source']
 
-    The current approaches we can think of are:
-        1.  Take the messageId (or another unique identifier) from the SES event and search the bucket for it
-            (It's worth mentioning that the documentation suggests there's no consistent format for them)
+    if not any(source.endswith(email_address) for email_address in permitted_emails):
+        raise EmailValidationError(f"Email not from permitted sender. Got: {source}")
 
-        2.  Using SNS (instead of SES)...
-                Take the entire message contents
-                decode the base64
-                pull the CSV out of that somehow
-            (This would potentially change the json-schema defined in validate_event() - beware)
-    '''
+    print(f"Source validation passed - email from permitted sender: {source}")
 
-def compress_csv(csv):
+def validate_event_destination(ses_mail: dict):
+    destination = ses_mail['destination']
 
+    if expected_destination not in destination:
+        raise EmailValidationError(f"Unexpected destination: {destination}")
 
-def store_file_in_s3(file):
+    print('Destination validation passed')
+
+def validate_event_headers(ses_mail: dict):
+    headers = {h["name"]: h["value"] for h in ses_mail["headers"]}
+    required_headers = {
+        "X-SES-Spam-Verdict": "PASS",
+        "X-SES-Virus-Verdict": "PASS",
+        "X-MS-Has-Attach": "Yes",
+    }
+
+    for key, expected in required_headers.items():
+        if headers.get(key) != expected:
+            raise EmailValidationError(f"Email validation failed. Header {key} did not pass. Got: {headers.get(key)}")
+
+    print('Header validation passed')
+
+def validate_event_receipt(ses_receipt: dict):
+    if not all([
+        ses_receipt['spamVerdict']['status'] == 'PASS',
+        ses_receipt['virusVerdict']['status'] == 'PASS',
+        ses_receipt['spfVerdict']['status'] == 'PASS', # TODO PRM-131 Is this expected?
+        ses_receipt['dkimVerdict']['status'] == 'PASS', # TODO PRM-131 Is this expected?
+        ses_receipt['dmarcVerdict']['status'] == 'PASS', # TODO PRM-131 Is this expected?
+    ]):
+        raise EmailValidationError("Email validation failed due to incorrect receipt")
+
+    print('Receipt validation passed')
+
+def remove_email_from_s3(email_event: dict):
+    source_s3_bucket = get_ssm_param(source_s3_bucket_ssm_location)
+    file_key = email_event['ses']['mail']['messageId']
+
+    try:
+        s3_client.delete_object(Bucket=source_s3_bucket, Key=file_key)
+        print(f"Deleted email from S3: bucket={source_s3_bucket}, key={file_key}")
+    except ClientError as e:
+        print(f"Failed to delete email from S3: bucket={source_s3_bucket}, key={file_key}, error={e}")
+
+def get_raw_email_from_source_s3(email_event: dict):
+    source_s3_bucket = get_ssm_param(source_s3_bucket_ssm_location)
+    file_key = email_event['ses']['mail']['messageId']
+
+    try:
+        response = s3_client.get_object(Bucket=source_s3_bucket, Key=file_key)
+        return response['Body'].read()
+    except ClientError as e:
+        raise RuntimeError(f"Failed to retrieve email from S3: bucket={source_s3_bucket}, key={file_key}, error={e}")
+
+def extract_csv_attachment_from_email(raw_email: bytes):
+    msg = email.message_from_bytes(raw_email)
+    for part in msg.walk():
+        if part.get_filename() == asid_lookup_filename:
+            return part.get_payload(decode=True)
+    else:
+        raise FileNotFoundError("asidLookup.csv not found in email")
+
+def compress_csv(csv: bytes):
+    output = BytesIO()
+    with gzip.GzipFile(fileobj=output, mode="wb") as gz:
+        gz.write(csv)
+
+    output.seek(0)
+
+    return output
+
+def store_file_in_destination_s3(file: BytesIO):
+    now = datetime.now(timezone.utc)
+    file_key = f"{now.year}/{now.month}/{asid_lookup_filename}.gz"
+    s3_client.put_object(Bucket=destination_s3_bucket, Key=file_key, Body=file.read())
+    print(f"Successfully uploaded compressed file at {destination_s3_bucket}/{file_key}")
+
+def get_ssm_param(name):
+    return ssm_client.get_parameter(Name=name, WithDecryption=True)['Parameter']['Value']
+
+class EmailValidationError(Exception):
+    pass
