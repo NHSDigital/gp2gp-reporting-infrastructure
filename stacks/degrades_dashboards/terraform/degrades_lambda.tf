f059536d1ea660611ce0d30c678c06c161dcedd1
Steph Torres <stephane.torres1@nhs.net>
2025-05-29 17:00:03 +0100

    [PRM-307] add api gateway resources for degrades lambda

--- stacks/degrades_dashboards/terraform/degrades_lambda.tf
+++ stacks/degrades_dashboards/terraform/degrades_lambda.tf
@@ -1,7 +1,15 @@
+terraform {
+  required_providers {
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "1.23.0"
+    }
+  }
+}
 resource "aws_lambda_function" "degrades_lambda" {
   filename         = var.degrades_lambda_zip_file
   function_name    = "${var.environment}_${var.degrades_lambda_name}"
-  role             = ""
+  role             = aws_iam_role.degrades_lambda_role.arn
   handler          = "main.lambda_handler"
   runtime          = "python3.12"
   source_code_hash = data.archive_file.degrades_lambda.output_base64sha256
@@ -30,4 +38,40 @@ data "archive_file" "degrades_lambda" {
   type        = "zip"
   source_dir  = "${path.module}/lambda/degrades_dashboards"
   output_path = var.degrades_lambda_zip_file
+}
+
+resource "aws_api_gateway_rest_api" "degrades_api" {
+  name        = "degrades_api"
+  description = "API for Degrades work"
+}
+
+resource "aws_api_gateway_deployment" "degrades_api_deploy" {
+  rest_api_id = aws_api_gateway_rest_api.degrades_api.id
+}
+
+resource "aws_api_gateway_stage" "degrades" {
+  rest_api_id = aws_api_gateway_rest_api.degrades_api.id
+  name        = "dev"
+}
+
+
+resource "aws_api_gateway_resource" "degrades" {
+  parent_id   = aws_api_gateway_rest_api.degrades_api.root_resource_id
+  path_part   = "degrades"
+  rest_api_id = aws_api_gateway_rest_api.degrades_api.id
+}
+
+resource "aws_api_gateway_method" "degrades_get" {
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.degrades.id
+  rest_api_id   = aws_api_gateway_rest_api.degrades_api.id
+}
+
+resource "aws_api_gateway_integration" "degrades_get" {
+  http_method = aws_api_gateway_method.degrades_get.http_method
+  resource_id = aws_api_gateway_resource.degrades.id
+  rest_api_id = aws_api_gateway_rest_api.degrades_api.id
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.degrades_lambda.invoke_arn
 }
