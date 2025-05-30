2b6e89aec9dbbbd8e9af66bc72a034cf6c14e7fb
Steph Torres <stephane.torres1@nhs.net>
2025-05-30 15:34:28 +0100

    [PRM-307] change source code hash for degrades

--- stacks/degrades_dashboards/terraform/degrades_lambda.tf
+++ stacks/degrades_dashboards/terraform/degrades_lambda.tf
@@ -4,13 +4,6 @@ resource "aws_lambda_function" "degrades_lambda" {
   role             = aws_iam_role.degrades_lambda_role.arn
   handler          = "main.lambda_handler"
   runtime          = "python3.12"
-  source_code_hash = data.archive_file.degrades_lambda.output_base64sha256
+  source_code_hash = filebase64sha256("${var.degrades_lambda_zip_file}")
   timeout          = 15
-}
-
-
-data "archive_file" "degrades_lambda" {
-  type        = "zip"
-  source_dir  = "${path.module}/lambda/degrades_dashboards"
-  output_path = var.degrades_lambda_zip_file
 }
