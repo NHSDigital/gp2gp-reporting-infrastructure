6d58523bc22ab25be6a7444c00b474ccaf575454
Steph Torres <stephane.torres1@nhs.net>
2025-06-03 08:30:25 +0100

    add test for retrieving files from S3

--- lambda/degrades-dashboards/main.py
+++ lambda/degrades-dashboards/main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import boto3
 from datetime import datetime
 from utils.decorators import validate_date_input
 from utils.utils import  get_key_from_date
@@ -7,6 +8,7 @@ from utils.utils import  get_key_from_date
 
 def get_files_from_S3(key):
     print(key)
+    return [1,2,3,4,5,6,7]
 
 @validate_date_input
 def lambda_handler(event, context):
--- lambda/degrades-dashboards/tests/test_degrade_lambda.py
+++ lambda/degrades-dashboards/tests/test_degrade_lambda.py
@@ -2,7 +2,7 @@ import os
 import boto3
 from moto import mock_aws
 
-from main import lambda_handler
+from main import lambda_handler, get_files_from_S3
 from tests.conftest import REGION_NAME, MOCK_BUCKET
 
 def test_lambda_handler_throws_400_no_query_string(mock_invalid_event_empty_query_string, context):
@@ -33,18 +33,28 @@ def test_lambda_handler_returns_200(mock_valid_event_valid_date, context):
     assert result == expected
 
 
-@mock_aws
 def test_lambda_handler_calls_S3_with_file_path(mock_valid_event_valid_date, context, mocker):
     mock_function_call = mocker.patch('main.get_files_from_S3')
-    # folder_path = 'tests/mocks/mixed_messages'
-    # json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
-    #
-    # conn = boto3.resource('s3', region_name=REGION_NAME)
-    # bucket = conn.create_bucket(Bucket=MOCK_BUCKET)
-    #
-    # for file in json_files:
-    #     bucket.upload_file(os.path.join(folder_path, file), f"2024/01/01/{file}")
+
 
     lambda_handler(mock_valid_event_valid_date, context)
 
     mock_function_call.assert_called_with("2024/01/01")
+
+
+@mock_aws
+def test_get_files_from_S3():
+    folder_path = 'tests/mocks/mixed_messages'
+    json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
+
+    conn = boto3.resource('s3', region_name=REGION_NAME)
+    bucket = conn.create_bucket(Bucket=MOCK_BUCKET)
+
+    for file in json_files:
+        bucket.upload_file(os.path.join(folder_path, file), f"2024/01/01/{file}")
+
+    files = get_files_from_S3("2024/01/01/")
+    assert len(files) == len(json_files)
+
+    for index in range(len(files)):
+        assert files[index] == f"tmp/2024/01/01/{json_files[index]}"
