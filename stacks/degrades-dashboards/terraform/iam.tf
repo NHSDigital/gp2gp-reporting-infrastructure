d0e6ddabaa66792e305d2158fb9730e496bb1397
Steph Torres <stephane.torres1@nhs.net>
2025-06-11 14:16:15 +0100

    [PRM-351] add read registrations mi events bucket policy to degrades lambda

--- stacks/degrades-dashboards/terraform/iam.tf
+++ stacks/degrades-dashboards/terraform/iam.tf
@@ -15,3 +15,27 @@ resource "aws_iam_role" "degrades_lambda_role" {
   name               = "${var.environment}_degrades_lambda_role"
   assume_role_policy = data.aws_iam_policy_document.degrades_lambda_assume_role.json
 }
+
+resource "aws_iam_role_policy_attachment" "degrades_lambda_s3_read" {
+  role       = aws_iam_role.degrades_lambda_role.name
+  policy_arn = aws_iam_policy.read_registrations_mi_events.arn
+}
+
+resource "aws_iam_policy" "read_registrations_mi_events" {
+  name   = "${var.environment}-${var.degrades_lambda_name}"
+  policy = data.aws_iam_policy_document.read_registrations_mi_events.json
+}
+
+data "aws_iam_policy_document" "read_registrations_mi_events" {
+  statement {
+    actions = [
+      "s3:Get*",
+      "s3:List*",
+      "s3:Describe*",
+      "s3-object-lambda:Get*",
+      "s3-object-lambda:List*"
+    ]
+    resources = [
+    "arn:aws:s3:::${var.registrations_mi_event_bucket}-${var.environment}/*"]
+  }
+}
