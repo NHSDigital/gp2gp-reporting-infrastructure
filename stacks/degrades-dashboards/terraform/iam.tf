852885810e7b9b978f27bbc6eb698c10c784d7cf
Steph Torres <stephane.torres1@nhs.net>
2025-06-16 16:36:36 +0100

    [PRM-358] add new infra to handle receiving degrades messages

--- stacks/degrades-dashboards/terraform/iam.tf
+++ stacks/degrades-dashboards/terraform/iam.tf
@@ -42,6 +42,11 @@ data "aws_iam_policy_document" "read_registrations_mi_events" {
 }
 
 # Degrades Message Receiver Lambda
+resource "aws_iam_role" "degrades_message_receiver_lambda" {
+  name = "degrades_message_receiver_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.degrades_message_receiver_lambda_assume_role.json
+}
+
 data "aws_iam_policy_document" "degrades_message_receiver_lambda_assume_role" {
   statement {
     effect = "Allow"
@@ -53,4 +58,24 @@ data "aws_iam_policy_document" "degrades_message_receiver_lambda_assume_role" {
       type = "Service"
     }
   }
+}
+
+resource "aws_iam_role_policy_attachment" "degrades_message_receiver_lambda_dynamodb" {
+  policy_arn = aws_iam_policy.degrades_message_table_access.arn
+  role       = aws_iam_role.degrades_message_receiver_lambda.name
+}
+
+resource "aws_iam_policy" "degrades_message_table_access" {
+  name = "degrades_table_access_policy"
+  policy = data.aws_iam_policy_document.degrades_message_table_access.json
+}
+
+resource "aws_iam_role_policy_attachment" "degrades_lambda_sqs_invoke" {
+  policy_arn = aws_iam_policy.degrades_lambda_sqs_invoke.arn
+  role       = aws_iam_role.degrades_message_receiver_lambda.name
+}
+
+resource "aws_iam_policy" "degrades_lambda_sqs_invoke" {
+  name   = "degrades_sqs_invoke_policy"
+  policy = data.aws_iam_policy_document.degrades_messages_sqs_receive.json
 }
--- stacks/degrades-dashboards/terraform/variables.tf
+++ stacks/degrades-dashboards/terraform/variables.tf
@@ -1,7 +1,13 @@
 variable "degrades_dashboards_api_lambda_zip" {
   type        = string
-  description = "File path for Degrades Lambda zip"
+  description = "File path for Degrades API Lambda zip"
   default     = "lambda/build/degrades-api-dashboards.zip"
+}
+
+variable "degrades_message_receiver_lambda_zip" {
+  type = string
+  description = "File path Degrades Message Receiver Lambda"
+  default = "lambda/build/degrades-message-receiver-lambda.zip"
 }
 
 variable "environment" {
@@ -10,9 +16,23 @@ variable "environment" {
 }
 
 variable "degrades_api_lambda_name" {
+  type = string
+  description = "Name of Degrades API lambda"
   default = "degrades_api_dashboards_lambda"
 }
 
+variable "degrades_message_receiver_lambda_name" {
+  type = string
+  description = "Name of Degrades Message Receiver Lambda"
+  default = "degrades_message_receiver_lambda"
+}
+
 variable "registrations_mi_event_bucket" {
+  type = string
   description = "Name of terraform state bucket"
+}
+
+variable "degrades_message_table" {
+  type = string
+  description = "Name of Degrades Message DynamoDB table"
 }
