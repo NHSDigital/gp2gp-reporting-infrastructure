dc52e73b8d3dce26c8f13f3c5c774c802585422b
Steph Torres <stephane.torres1@nhs.net>
2025-06-17 15:23:37 +0100

    [PRM-358] add test file for degrades message receiver lambda

---
+++ lambda/degrades-dashboards/tests/test_degrades_message_receiver_lambda.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+from moto import mock_aws
+from degrades_message_receiver.main import lambda_handler
+from tests.conftest import REGION_NAME, MOCK_DEGRADES_MESSAGE_TABLE_NAME, MOCK_DEGRADES_MESSAGE_TABLE_KEY_SCHEMA, \
+    MOCK_DEGRADES_MESSAGE_TABLE_ATTRIBUTES, MOCK_VALID_DEGRADES_MESSAGE
+
+
+@mock_aws
+def test_degrades_message_receiver_puts_item_on_table(set_env, context):
+    conn = boto3.resource("dynamodb", region_name=REGION_NAME)
+    degrades_table = conn.create_table(TableName=MOCK_DEGRADES_MESSAGE_TABLE_NAME,
+                                       KeySchema=MOCK_DEGRADES_MESSAGE_TABLE_KEY_SCHEMA,
+                                       AttributeDefinitions=MOCK_DEGRADES_MESSAGE_TABLE_ATTRIBUTES,
+                                       BillingMode="PAY_PER_REQUEST", )
+
+    event = {"Records": [{"body": json.dumps(MOCK_VALID_DEGRADES_MESSAGE)}]}
+
+    lambda_handler(event, context)
+
+    actual = degrades_table.get_item(Key={"Timestamp": MOCK_VALID_DEGRADES_MESSAGE["eventGeneratedDateTime"],
+                                          "MessageID": MOCK_VALID_DEGRADES_MESSAGE["eventId"]})
+    assert actual["Item"] == MOCK_VALID_DEGRADES_MESSAGE
