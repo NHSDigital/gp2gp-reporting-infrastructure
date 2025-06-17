7cff22f05623f4a015d16c1769757a228bd52403
Steph Torres <stephane.torres1@nhs.net>
2025-06-17 15:21:49 +0100

    [PRM-358] set up mocks for testing new lambda

--- lambda/degrades-dashboards/tests/conftest.py
+++ lambda/degrades-dashboards/tests/conftest.py
@@ -5,6 +5,87 @@ import pytest
 MOCK_INTERACTION_ID = "88888888-4444-4444-4444-121212121212"
 REGION_NAME = "us-east-1"
 MOCK_BUCKET = "test-s3-bucket"
+MOCK_DEGRADES_MESSAGE_TABLE_NAME = "degrades_messages_table"
+
+MOCK_DEGRADES_MESSAGE_TABLE_ATTRIBUTES = [
+    {
+        'AttributeName': 'Timestamp',
+        'AttributeType': 'N'
+    },
+    {
+        'AttributeName': 'MessageID',
+        'AttributeType': 'S'
+    },
+]
+
+MOCK_DEGRADES_MESSAGE_TABLE_KEY_SCHEMA = [
+    {
+        'AttributeName': 'Timestamp',
+        'KeyType': 'HASH'
+    },
+    {
+        "AttributeName": "MessageID",
+        "KeyType": "RANGE"
+    }
+]
+
+MOCK_VALID_DEGRADES_MESSAGE = {
+    "eventId": "02-DEGRADES-02",
+    "eventGeneratedDateTime": "2024-09-20T00:00:00",
+    "eventType": "DEGRADES",
+    "reportingSystemSupplier": "EMIS",
+    "payload": {
+        "degrades": [
+            {
+                "type": "MEDICATION",
+                "reason": "CODE",
+                "coding": [
+                    {
+                        "code": "01147001",
+                        "system": "UNKNOWN"
+                    },
+                    {
+                        "code": "01147001",
+                        "system": "UNKNOWN"
+                    }
+                ]
+            },
+            {
+                "type": "RECORD_ENTRY",
+                "reason": "CODE",
+                "coding": [
+                    {
+                        "code": "oaz1.",
+                        "system": "CLINICAL_TERMS_READV3"
+                    },
+                    {
+                        "code": "x001v",
+                        "system": "CLINICAL_TERMS_READV3"
+                    },
+                    {
+                        "code": "ga8..",
+                        "system": "CLINICAL_TERMS_READV3"
+                    }
+                ]
+            },
+            {
+                "type": "NON_DRUG_ALLERGY",
+                "reason": "CODE",
+                "coding": [
+                    {
+                        "code": "196471000000108",
+                        "system": "SNOMED_CT"
+                    },
+                    {
+                        "code": "609328004",
+                        "system": "SNOMED_CT"
+                    }
+                ]
+            }
+        ]
+    }
+}
+
 
 @pytest.fixture
 def mock_invalid_event_empty_query_string():
@@ -16,6 +97,7 @@ def mock_invalid_event_empty_query_string():
 
     return api_gateway_event
 
+
 @pytest.fixture
 def mock_invalid_event_without_date():
     api_gateway_event = {
@@ -25,6 +107,7 @@ def mock_invalid_event_without_date():
     }
 
     return api_gateway_event
+
 
 @pytest.fixture
 def mock_invalid_event_invalid_date_format():
@@ -36,6 +119,7 @@ def mock_invalid_event_invalid_date_format():
 
     return api_gateway_event
 
+
 @pytest.fixture
 def mock_valid_event_valid_date():
     api_gateway_event = {
@@ -44,6 +128,7 @@ def mock_valid_event_valid_date():
         "headers": {},
     }
     return api_gateway_event
+
 
 @pytest.fixture
 def context():
@@ -62,3 +147,4 @@ def context():
 def set_env(monkeypatch):
     monkeypatch.setenv("AWS_REGION", REGION_NAME)
     monkeypatch.setenv("REGISTRATIONS_MI_EVENT_BUCKET", MOCK_BUCKET)
+    monkeypatch.setenv("DEGRADES_MESSAGE_TABLE", MOCK_DEGRADES_MESSAGE_TABLE_NAME)
--- lambda/degrades-dashboards/utils/utils.py
+++ lambda/degrades-dashboards/utils/utils.py
@@ -9,7 +9,7 @@ def calculate_number_of_degrades(path: str, files: list[str]) -> int:
 
     for file_name in files:
         file_path = os.path.join(path, file_name)
-        with open(file_path, "r") as json_file:
+        with open(file_path, "rb") as json_file:
             data = json.load(json_file)
             eventType = data.get("eventType", None)
             if eventType is not None and eventType == "DEGRADES":
