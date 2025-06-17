393aebac171bbe168f841a7a10e1efa0aa2a92eb
Steph Torres <stephane.torres1@nhs.net>
2025-06-17 16:03:41 +0100

    [PRM-358] handler puts item on table, incorrect format

--- lambda/degrades-dashboards/degrades_message_receiver/main.py
+++ lambda/degrades-dashboards/degrades_message_receiver/main.py
@@ -1,7 +1,16 @@
 import json
+import os
+import boto3
+from datetime import datetime
+DEGRADES_TABLE = os.getenv("DEGRADES_MESSAGE_TABLE")
+REGION = os.getenv("AWS_REGION")
+
 
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    message = json.loads(event["Records"][0]["body"])
+
+    timestamp = int(datetime.fromisoformat(message["eventGeneratedDateTime"]).timestamp())
+
+    client = boto3.client("dynamodb", region_name=os.getenv("AWS_REGION"))
+    client.put_item(TableName=os.getenv("DEGRADES_MESSAGE_TABLE"), Item={"MessageID": {"S": message["eventId"]},
+                                                                         "Timestamp": {"N": f"{timestamp}"}})
--- lambda/degrades-dashboards/tests/test_degrades_message_receiver_lambda.py
+++ lambda/degrades-dashboards/tests/test_degrades_message_receiver_lambda.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+from datetime import datetime
 from moto import mock_aws
 from degrades_message_receiver.main import lambda_handler
 from tests.conftest import REGION_NAME, MOCK_DEGRADES_MESSAGE_TABLE_NAME, MOCK_DEGRADES_MESSAGE_TABLE_KEY_SCHEMA, \
@@ -17,7 +18,8 @@ def test_degrades_message_receiver_puts_item_on_table(set_env, context):
     event = {"Records": [{"body": json.dumps(MOCK_VALID_DEGRADES_MESSAGE)}]}
 
     lambda_handler(event, context)
+    timestamp = int(datetime.fromisoformat(MOCK_VALID_DEGRADES_MESSAGE["eventGeneratedDateTime"]).timestamp())
 
-    actual = degrades_table.get_item(Key={"Timestamp": MOCK_VALID_DEGRADES_MESSAGE["eventGeneratedDateTime"],
+    actual = degrades_table.get_item(Key={"Timestamp": timestamp,
                                           "MessageID": MOCK_VALID_DEGRADES_MESSAGE["eventId"]})
     assert actual["Item"] == MOCK_VALID_DEGRADES_MESSAGE
