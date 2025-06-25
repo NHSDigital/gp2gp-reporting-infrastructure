f41dcba26f6c27f7f7abbe26587aa1346922a8ca
Steph Torres <stephane.torres1@nhs.net>
2025-06-25 14:28:23 +0100

    [PRM-366] add cloudwatch logs to daily summary lambda

--- stacks/degrades-dashboards/terraform/cloudwatch.tf
+++ stacks/degrades-dashboards/terraform/cloudwatch.tf
@@ -3,6 +3,7 @@ resource "aws_cloudwatch_log_group" "degrades_messages_receiver" {
   retention_in_days = 14
 }
 
-resource "aws_cloudwatch_log_group" "degrades_daily_summary_lambda" {
-
+resource "aws_cloudwatch_log_group" "degrades_daily_summary" {
+  name              = "/aws/lambda/${aws_lambda_function.degrades_daily_summary_lambda.function_name}"
+  retention_in_days = 14
 }
--- stacks/degrades-dashboards/terraform/degrades-daily-summary-iam.tf
+++ stacks/degrades-dashboards/terraform/degrades-daily-summary-iam.tf
@@ -15,3 +15,29 @@ data "aws_iam_policy_document" "degrades_daily_summary_lambda_assume_role" {
     }
   }
 }
+
+# Cloudwatch Logging
+data "aws_iam_policy_document" "degrade_daily_summary_lambda_logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["${aws_cloudwatch_log_group.degrades_daily_summary.arn}", "${aws_cloudwatch_log_group.degrades_daily_summary.arn}:*"]
+  }
+}
+
+resource "aws_iam_policy" "degrades_daily_summary_logging" {
+  name   = "degrades_daily_summary_lambda_logging_policy"
+  policy = data.aws_iam_policy_document.degrade_daily_summary_lambda_logging.json
+}
+
+resource "aws_iam_role_policy_attachment" "degrades_daily_summary_logging" {
+  role       = aws_iam_role.degrades_daily_summary_lambda.name
+  policy_arn = aws_iam_policy.degrades_daily_summary_logging.arn
+}
+
