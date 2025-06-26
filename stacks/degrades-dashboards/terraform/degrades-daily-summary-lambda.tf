99e0f7713ee713a244dcd69bb3dbdf287074397b
Steph Torres <stephane.torres1@nhs.net>
2025-06-26 13:00:35 +0100

    [PRM-366] add schedule to degrades daily summary lambda

--- stacks/degrades-dashboards/terraform/degrades-daily-summary-lambda.tf
+++ stacks/degrades-dashboards/terraform/degrades-daily-summary-lambda.tf
@@ -8,8 +8,21 @@ resource "aws_lambda_function" "degrades_daily_summary_lambda" {
   source_code_hash = filebase64sha256("${var.degrades_daily_summary_lambda_zip}")
 }
 
-# resource "aws_lambda_permission" "degrades_daily_summary_lambda" {
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.degrades_daily_summary_lambda.function_name
-#   principal     = ""
-# }
+resource "aws_lambda_permission" "degrades_daily_summary_lambda" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.degrades_daily_summary_lambda.function_name
+  principal     = "eventsa.amazonaws.com"
+  source_arn    = ""
+}
+
+resource "aws_cloudwatch_event_rule" "degrades_daily_summary_lambda_schedule" {
+  name        = "${var.environment}_${var.degrades_daily_summary_lambda_name}_schedule"
+  description = "Schedule for Degrades Daily Summary Lambda"
+  schedule_expression = "cron(0 6 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.degrades_daily_summary_lambda_schedule.name
+  target_id = "degrades_daily_summary_lambda_schedule"
+  arn       = aws_lambda_function.degrades_daily_summary_lambda.arn
+}
