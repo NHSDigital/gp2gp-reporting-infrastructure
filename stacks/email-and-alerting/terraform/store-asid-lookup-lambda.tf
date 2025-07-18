5698c01a905a4510514a74ffe6ec2d47455c9be8
chrisbloe-nhse <151756494+chrisbloe-nhse@users.noreply.github.com>
2025-07-18 09:41:13 +0100

    [PRM-152] Add lambda:InvokeFunction permission for ses.amazonaws.com (asid-lookup-lambda)

--- stacks/email-and-alerting/terraform/store-asid-lookup-lambda.tf
+++ stacks/email-and-alerting/terraform/store-asid-lookup-lambda.tf
@@ -18,3 +18,11 @@ resource "aws_cloudwatch_log_group" "store_asid_lookup" {
   name              = "/aws/lambda/${aws_lambda_function.store_asid_lookup.function_name}"
   retention_in_days = 0
 }
+
+resource "aws_lambda_permission" "store_asid_lookup_ses_trigger" {
+  statement_id  = "AllowExecutionFromSES"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.store_asid_lookup.function_name
+  principal     = "ses.amazonaws.com"
+  source_arn    = aws_ses_receipt_rule.asid_lookup.arn
+}
