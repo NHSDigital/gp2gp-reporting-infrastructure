e2b5cd99211b7b6175034984605abe8315c789f5
Kris Bloe <kris.bloe@answerdigital.com>
2025-07-18 12:11:06 +0100

    [PRM-152] aws_lambda_permission is now created before the aws_ses_receipt_rule

--- stacks/email-and-alerting/terraform/common.tf
+++ stacks/email-and-alerting/terraform/common.tf
@@ -14,3 +14,7 @@ data "aws_route53_zone" "gp_registrations" {
   name         = var.hosted_zone_name
   private_zone = false
 }
+
+locals {
+  ses_receipt_rule_asid_lookup_name = "store-asid-lookup-in-s3-${var.environment}"
+}
