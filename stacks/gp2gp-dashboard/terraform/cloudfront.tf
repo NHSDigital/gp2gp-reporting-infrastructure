662b0bc661c527e2e01f5de964310109f2f9dd7c
samfallowfield <73955537+samfallowfield@users.noreply.github.com>
2025-10-22 12:34:56 +0100

    [PRM-576] Remove deprecation warning for aws cloudfront distribution website endpoint

--- stacks/gp2gp-dashboard/terraform/cloudfront.tf
+++ stacks/gp2gp-dashboard/terraform/cloudfront.tf
@@ -13,7 +13,7 @@ resource "aws_cloudfront_distribution" "dashboard_s3_distribution" {
   aliases = [var.alternate_domain_name]
 
   origin {
-    domain_name = aws_s3_bucket_website_configuration.dashboard_website.website_endpoint
+    domain_name = aws_s3_bucket_website_configuration.dashboard_website.bucket_regional_domain_name
     origin_id   = local.s3_origin_id
 
     custom_origin_config {
