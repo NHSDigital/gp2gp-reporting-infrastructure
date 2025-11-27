7766370a9c93b36c94cbce90327f8519ad73eb5e
chrisbloe <kris.bloe2@nhs.net>
2025-11-27 14:17:31 +0000

    Adding some check exclusions

--- scripts/markdown-validator.sh
+++ scripts/markdown-validator.sh
@@ -12,6 +12,7 @@
 # `davidanson.vscode-markdownlint` that is already specified in the `.vscode/extensions.json` file.
 
 files=$((git diff --diff-filter=ACMRT --name-only origin/${BRANCH_NAME:-main}.. "*.md"; git diff --name-only "*.md") | sort | uniq)
+#files=$((find . -name "*.md" -printf '%P\n') | sort | uniq)
 
 if [ -n "$files" ]; then
   image=ghcr.io/igorshubovych/markdownlint-cli@sha256:b771097d3d9ec6b368fefcea5f0c05c8710293d9251fca17e31c8e10987b50ef # v0.46.0
@@ -19,5 +20,5 @@ if [ -n "$files" ]; then
     -v $PWD:/workdir \
     $image \
       $files \
-      --disable MD013 MD033
+      --disable MD013 MD033 MD060 # MD060 disabled until VS Code support is available
 fi
